e962c7c49a6feb823f16258c6e998b1d7ca017f5
Gene He <genehe3@gmail.com>
2021-11-10 06:53:03 -0800

    redo

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//redo of first attempt
+
+let board = document.querySelector('canvas');
+let ctx = board.getContext('2d');
+
+ctx.fillStyle = "rgb(200, 0, 0)";
+ctx.fillRect(900, 400, 30, 30)
 
 
 
